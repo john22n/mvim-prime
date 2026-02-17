8cb147f14a390747c948698c54e9a27be43d47a5
John Noriega <johnnoriega22@icloud.com>
2026-02-16 18:43:16 -0700

    fix ts-error translator config

--- lua/theprimeagen/lazy/ts-error-translator.lua
+++ lua/theprimeagen/lazy/ts-error-translator.lua
@@ -3,16 +3,7 @@ return {
     "dmmulroy/ts-error-translator.nvim",
     ft = { "typescript", "typescriptreact", "javascript", "javascriptreact" },
     config = function()
-      -- Only activate when tsserver attaches
-      vim.api.nvim_create_autocmd("LspAttach", {
-        callback = function(args)
-          local client = vim.lsp.get_client_by_id(args.data.client_id)
-          if client and client.name == "tsserver" then
-            require("ts-error-translator").setup({})
-          end
-        end,
-      })
+      require("ts-error-translator").setup({})
     end,
   },
 }
-
